3e16d7c9ad6bdbe0b5873734708a12437358745c
Shayala Chowdhury <cshayala@gmail.com>
2017-03-29 20:31:26 +0000

    Shape Functions

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(length){
+forward(length);
 right(90);
-forward(50);
+forward(length);
 right(90);
-forward(50);
+forward(length);
 right(90);
-forward(50);
+forward(length);
 right(90);
-forward(50);
+forward(length);
 }
 
-// Type your function call below
+function rectangle(length, height){
+forward(length);
+right(90);
+forward(height);
+right(90);
+forward(length);
+right(90);
+forward(height);
+right(90);
 
+}
+
+
+// Type your function call below
+function triangle(length){
+forward(length); 
+right(120);
+forward(length); 
+right(120);
+forward(length); 
+right(120);
+}
+
+//triangle(20);
+//triangle(80);
+
+//square(40);
+//square(60);
+
+rectangle(90, 70);
 
 stamp();
